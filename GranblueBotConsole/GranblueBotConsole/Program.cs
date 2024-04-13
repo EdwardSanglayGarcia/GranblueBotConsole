74df1f36cfcce846aa466ee647be5aac34761a96
Edward Garcia <edward_12_1998@yahoo.com>
2024-04-13 13:18:21 +0800

    Added Major Removal

--- GranblueBotConsole/GranblueBotConsole/Program.cs
+++ GranblueBotConsole/GranblueBotConsole/Program.cs
@@ -17,7 +17,7 @@ namespace GranblueBotConsole
 
             Clicker.Testing(Clicker.Action.LeftButtonDown, 101, 95);
             Clicker.Testing(Clicker.Action.LeftButtonUp, 101, 95);
-            Thread.Sleep(2000);
+            Thread.Sleep(4000);
             Clicker.Testing(Clicker.Action.LeftButtonDown, 236, 253);
             Clicker.Testing(Clicker.Action.LeftButtonUp, 236, 253);
 
